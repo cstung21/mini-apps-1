06735a7e21f8671fd9e7cc05f637fbfd4ee87e95
Christopher Tung <cstung21@gmail.com>
2018-08-29 08:58:27 -0700

    csv generator links client to server

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,58 @@
+// $(document).ready(function() {
+  
+//   $(".form").submit(function(event) {
+//     event.preventDefault();
+//     let $body = $(".textarea").val();
+//     $(".textarea").val(''); 
+//     console.log('body', $body);
+    
+//     $.ajax({
+//       url: 'http://localhost:3000/csv',
+//       method: 'POST',
+//       data: JSON.stringify({$body}),
+//       contentType: 'application/json',
+//       error: () => {
+//         console.log('error occurred for ajax post req');
+//       },
+//       success: () => {
+//         console.log('ajax post req successful');
+//       }
+//     });
+//   });
+
+// });
+
+//refactored
+var app = {
+
+  init: () => {
+    $(".form").submit(function(event) {
+      event.preventDefault();
+      var $text = $(".textarea").val();
+      $(".textarea").val('');
+      app.initiateAjaxPost($text);
+    });
+  },
+
+  initiateAjaxPost: (input) => {
+    $.ajax({
+      url: 'http://localhost:3000/csv',
+      method: 'POST',
+      data: JSON.stringify({input}),
+      contentType: 'application/json',
+      error: () => {
+        console.log('error occurred for ajax post req');
+      },
+      success: () => {
+        console.log('ajax post req successful');
+      }
+    });
+  }
+};
+
+
+$(document).ready(function() {
+  app.init();
+});
 
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,31 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, '/client')));
+
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 
+app.post('/csv', bodyParser.json(), function(req, res) {
+  console.log('POST request has been received!');
+  console.log(JSON.parse(req.body.input));
+  console.log(typeof JSON.parse(req.body.input))
+
+  // if (err) {
+  //   console.log('Error with post request from post handler');
+  // } else {
+  // var parsedInput = JSON.parse(req.body.input);
+  // console.log('PARSED BODY:', parsedInput)
+  res.status(200).send('post success');
+  // }
+});
+
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
