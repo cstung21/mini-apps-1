c3c8c4a8bc6d7b23c74d6d7412528c00a964256f
Christopher Tung <cstung21@gmail.com>
2018-09-02 20:40:27 -0700

    complete rewrite of Home and Account pages, consolidated state into App component, rewrote database schema to have one table for all user details

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -2,68 +2,59 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      home: true,
-      account: false,
-      shipping: false,
-      billing: false,
-      summary: false
+      currentPage: 0,
+      name: '',
+      email: '',
+      password: '',
+      address1: '',
+      address2: '',
+      city: '',
+      state: '',
+      zipcode: '',
+      phone: '',
+      creditCard: '',
+      expiration: '',
+      cvv: '',
+      billingZipcode: ''
     };
 
-    this.handleCheckoutClick = this.handleCheckoutClick.bind(this);
-    this.handleCreateAccountClick = this.handleCreateAccountClick.bind(this);
-    this.handleShippingClick = this.handleShippingClick.bind(this);
-    this.handleBillingClick = this.handleBillingClick.bind(this);
-    this.handlePurchaseClick = this.handlePurchaseClick.bind(this);
+  this.handleClick = this.handleClick.bind(this); 
+  this.handleInputChange = this.handleInputChange.bind(this);
   }
 
-  handleCheckoutClick() {
-    this.setState({
-      home: false,
-      account: true
-    });
+  handleClick(prevState, props) {
+    if (this.state.currentPage === 4) {
+      this.setState({
+        currentPage: 0
+      });
+    } else {
+      this.setState((prevState, props) => ({
+        currentPage: prevState.currentPage + 1
+      }));
+    }
   }
 
-  handleCreateAccountClick() {
+  handleInputChange(event) {
+    const stateProp = event.target.name;
     this.setState({
-      account: false,
-      shipping: true
+      [stateProp]: event.target.value
     });
-  }
-
-  handleShippingClick() {
-    this.setState({
-      shipping: false,
-      billing: true
-    });
-  }
-
-  handleBillingClick() {
-    this.setState({
-      billing: false,
-      summary: true
-    });
-  }
-
-  handlePurchaseClick() {
-    this.setState({
-      summary: false,
-      home: true
-    });
+    console.log(this.state[stateProp]);
   }
 
   render() {
     let renderComponent;
 
-    if (this.state.home) {
-      renderComponent = <Home handleCheckoutClick={this.handleCheckoutClick} />;
-    } else if (this.state.account) {
-      renderComponent = <Account handleCreateAccountClick={this.handleCreateAccountClick} />;
-    } else if (this.state.shipping) {
-      renderComponent = <Shipping handleShippingClick={this.handleShippingClick} />;
-    } else if (this.state.billing) {
-      renderComponent = <Billing handleBillingClick={this.handleBillingClick} />;
-    } else if (this.state.summary) {
-      renderComponent = <Summary handlePurchaseClick={this.handlePurchaseClick} />;
+    if (this.state.currentPage === 0) {
+      renderComponent = <Home handleClick={this.handleClick} />;
+    } else if (this.state.currentPage === 1) {
+      renderComponent = <Account handleClick={this.handleClick} handleInputChange={this.handleInputChange}/>;
+    } else if (this.state.currentPage === 2) {
+      renderComponent = <Shipping handleClick={this.handleClick} handleInputChange={this.handleInputChange}/>;
+    } else if (this.state.currentPage === 3) {
+      renderComponent = <Billing handleClick={this.handleClick} handleInputChange={this.handleInputChange}/>;
+    } else if (this.state.currentPage === 4) {
+      renderComponent = <Summary handleClick={this.handleClick} summary={this.state}/>;
     }
 
     return renderComponent;
@@ -76,87 +67,46 @@ function Home(props) {
   return (
     <div>
         <h1>Shopping Cart Checkout</h1>
-        <button onClick={props.handleCheckoutClick}>
+        <button onClick={props.handleClick}>
           Checkout
         </button>
     </div>
   );
 }
 
-class Account extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state={
-      name: '',
-      email: '',
-      password: ''
-    };
-
-    this.handleNameChange = this.handleNameChange.bind(this);
-    this.handleEmailChange = this.handleEmailChange.bind(this);
-    this.handlePasswordChange = this.handlePasswordChange.bind(this);
-    this.handleCreateAccount = this.handleCreateAccount.bind(this);
-  }
-
-  handleNameChange(event) {
-    this.setState({
-      name: event.target.value
-    });
-  }
-
-  handleEmailChange(event) {
-    this.setState({
-      email: event.target.value
-    })
-  }
-
-  handlePasswordChange(event) {
-    this.setState({
-      password: event.target.value
-    });
-  }
-
-  handleCreateAccount(event) {
-    event.preventDefault();
-
-    let accountDetails = {
-      name: this.state.name,
-      email: this.state.email,
-      password: this.state.password
-    };
-
-    $.ajax({
-      url: 'http://localhost:3000/account',
-      method: 'POST',
-      contentType: 'application/JSON',
-      data: JSON.stringify(accountDetails),
-      error: function() {
-        console.log('ERROR: Account was NOT created...');
-      },
-      success: function(data, results) {
-        console.log('Account successfully created!');
-        console.log('Data:', data);
-      }
-    });
-    this.props.handleCreateAccountClick();
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Create Account</h1>
-        <form onSubmit={this.handleCreateAccount}>
-          <label> Name </label>
-            <input type="text" onChange={this.handleNameChange} />
-          <label> Email </label>
-            <input type="text" onChange={this.handleEmailChange} />
-          <label> Password </label>
-            <input type="text" onChange={this.handlePasswordChange} /> 
-          <input type="submit" value="Next" />  
-        </form>
-      </div>
-    );
-  }
+function Account(props) {
+  return (
+    <div>
+      <h1>Create Account</h1>
+      <form>
+        <label> Name 
+          <input 
+            type="text" 
+            name="name" 
+            onChange={props.handleInputChange} />
+        </label>
+        <br />
+        <label> Email 
+          <input 
+            type="text" 
+            name="email" 
+            onChange={props.handleInputChange} />
+        </label>
+        <br />
+        <label> Password 
+          <input 
+            type="text" 
+            name="password" 
+            onChange={props.handleInputChange} />
+        </label>
+        <br />
+        <input 
+          type="button" 
+          value="Next" 
+          onClick={props.handleClick} />  
+      </form>
+    </div>
+  );
 }
 
 function Shipping(props) {
